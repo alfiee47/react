1112335e30b5ad79b88271f262e04f4dfb0060a9
alfiee47 <alfredagyeimarfo@gmail.com>
2021-07-27 16:27:13 -0400

    added redux form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { BrowserRouter } from 'react-router-dom';
 import Main from './components/MainComponent'
 import './App.css';
 import {DISHES} from './shared/dishes'
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 class App extends Component {
   constructor(props){
@@ -14,11 +18,13 @@ class App extends Component {
   }
   render() {
     return (
+      <Provider store={store}>
       <BrowserRouter>
-      <div className="App">
-        <Main />
-      </div>
+        <div className="App">
+          <Main />
+        </div>
       </BrowserRouter>
+    </Provider>
     );
   }
 }
